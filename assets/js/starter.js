156464fc23944b0a1fae357b6f08b6cc283fa1e3
Francesco Brachini <gunner19751@gmail.com>
2020-12-14 13:16:00 +0100

    first commit

--- assets/js/starter.js
+++ assets/js/starter.js
@@ -18,5 +18,12 @@
 // If you're importing tooltips or popovers, be sure to include our Popper.js dependency
 // import "../../node_modules/popper.js/dist/popper.min.js";
 
-import "../../node_modules/bootstrap/js/dist/util.js";
-import "../../node_modules/bootstrap/js/dist/modal.js";
+// import "../../node_modules/bootstrap/js/dist/util.js";
+// import "../../node_modules/bootstrap/js/dist/modal.js";
+import "../../node_modules/jquery/dist/jquery.slim.js";
+import "../../node_modules/slick-carousel/slick/slick.min.js";
+import "../../node_modules/bootstrap/dist/js/bootstrap.min.js";
+
+
+
+
